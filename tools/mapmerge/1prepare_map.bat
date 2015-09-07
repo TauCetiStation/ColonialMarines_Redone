391aedb822c6485c83c12e67bbae7b24402e4c0a
Zve <tagmodez@gmail.com>
2015-09-08 02:07:28 +0300

    switching back to tg map merger.

--- tools/mapmerge/1prepare_map.bat
+++
@@ -1,8 +0,0 @@
-SET z_levels=7
-cd ../../_maps
-
-FOR /L %%i IN (1,1,%z_levels%) DO (
-  copy cmr-z%%i.dmm cmr-z%%i.dmm.backup
-)
-
-pause
--- tools/mapmerge/2clean_map.bat
+++
@@ -1,8 +0,0 @@
-SET z_levels=7
-cd 
-
-FOR /L %%i IN (1,1,%z_levels%) DO (
-  java -jar MapPatcher.jar -clean ../../_maps/cmr-z%%i.dmm.backup ../../_maps/cmr-z%%i.dmm ../../_maps/cmr-z%%i.dmm
-)
-
-pause
